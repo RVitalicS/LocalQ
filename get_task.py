86b77dc374aaea62feea5f9c42ecde4c650c3774
RVitalicS <myhotstamp@hotmail.com>
2019-12-08 03:24:06 +0300

    Settings was expanded

--- get_task.py
+++ get_task.py
@@ -11,7 +11,7 @@ import task_translator
 
 
 # get settings data
-settings_path = os.path.join(os.path.dirname(__file__), "settings.json")
+settings_path = os.path.join(os.path.dirname(__file__), 'settings.json')
 settings_data = json_manager.read(settings_path)
 
 
@@ -19,16 +19,16 @@ settings_data = json_manager.read(settings_path)
 
 
 # get slave name
-slave = os.getenv("COMPUTERNAME")
+slave = os.getenv('COMPUTERNAME')
 
 
 # exit point
-if "ALL" in settings_data["exit"]: sys.exit()
-if slave in settings_data["exit"]: sys.exit()
+if 'ALL' in settings_data['exit']: sys.exit()
+if slave in settings_data['exit']: sys.exit()
 
 
 # work on free hours only
-if settings_data["timeException"]:
+if settings_data['timeException']:
     
     day     = time.localtime().tm_wday
     hours   = time.localtime().tm_hour
@@ -49,23 +49,23 @@ if settings_data["timeException"]:
 
 
 # create history path
-history_path = os.path.join(os.path.dirname(__file__), "history.json")
+history_path = os.path.join(os.path.dirname(__file__), 'history.json')
 
 
 # figure out next command
-for task in settings_data["tasks"]:
+for task in settings_data['tasks']:
 
 
     # extract task data
-    comments        = task["comments"]
-    katanaFile      = task["katanaFile"]
-    renderNode      = task["renderNode"]
-    var             = task["var"]
-    frameFrom       = task["frameFrom"]
-    frameTo         = task["frameTo"]
-    tileRender      = task["tileRender"]
-    allowedSlaves   = task["allowedSlaves"]
-    forbiddenSlaves = task["forbiddenSlaves"]
+    comments        = task['comments']
+    katanaFile      = task['katanaFile']
+    renderNode      = task['renderNode']
+    var             = task['var']
+    frameFrom       = task['frameFrom']
+    frameTo         = task['frameTo']
+    tileRender      = task['tileRender']
+    allowedSlaves   = task['allowedSlaves']
+    forbiddenSlaves = task['forbiddenSlaves']
 
 
     # define frame item
@@ -73,14 +73,14 @@ for task in settings_data["tasks"]:
         frame=0,
         tiles=None,
         complete=False,
-        time="",
+        time='',
         slaves=[slave],
         errors=[])
 
     tile=None
     if tileRender:
-        tileResolution = settings_data["tileResolution"]
-        raws = tileResolution["raws"]
+        tileResolution = settings_data['tileResolution']
+        raws = tileResolution['raws']
 
         tile_structure = [ [] for i in range(raws) ]
         tile = tile_manager.get_next(tile_structure)
@@ -89,7 +89,7 @@ for task in settings_data["tasks"]:
         raw    = tile[1]
         
         tile_structure[raw].append(column) 
-        frame_item["tiles"] = tile_structure
+        frame_item['tiles'] = tile_structure
 
 
     # check permissions
@@ -112,10 +112,10 @@ for task in settings_data["tasks"]:
 
     for history_task in history_data:
 
-        history_katanaFile = history_task["katanaFile"]
-        history_renderNode = history_task["renderNode"]
-        history_var        = history_task["var"]
-        history_frames     = [i["frame"] for i in history_task["frames"]]
+        history_katanaFile = history_task['katanaFile']
+        history_renderNode = history_task['renderNode']
+        history_var        = history_task['var']
+        history_frames     = [i['frame'] for i in history_task['frames']]
 
         match_file = katanaFile==history_katanaFile
         match_node = renderNode==history_renderNode
@@ -125,10 +125,10 @@ for task in settings_data["tasks"]:
 
 
             if tileRender:
-                for history_frame in history_task["frames"]:
+                for history_frame in history_task['frames']:
 
-                    if not history_frame["complete"]:
-                        history_tiles = history_frame["tiles"]
+                    if not history_frame['complete']:
+                        history_tiles = history_frame['tiles']
 
                         next_tile = tile_manager.get_next(history_tiles)
 
@@ -139,12 +139,12 @@ for task in settings_data["tasks"]:
                             history_tiles[raw].append(column)
                             for raw in history_tiles: raw=raw.sort()
 
-                            history_task["frames"] = sorted(history_task["frames"], key=lambda k: k["frame"])
+                            history_task['frames'] = sorted(history_task['frames'], key=lambda k: k['frame'])
 
-                            if slave not in history_frame["slaves"]:
-                                history_frame["slaves"].append(slave)
+                            if slave not in history_frame['slaves']:
+                                history_frame['slaves'].append(slave)
 
-                            render_frame = history_frame["frame"]
+                            render_frame = history_frame['frame']
 
                             json_manager.write(history_path, history_data)
                             print(task_translator.get_command(task, render_frame, tile=next_tile))
@@ -155,12 +155,12 @@ for task in settings_data["tasks"]:
             while frameFrom <= frameTo:
                 if frameFrom not in history_frames:
 
-                    frame_item["frame"] = frameFrom
+                    frame_item['frame'] = frameFrom
 
-                    history_task["frames"].append(frame_item)
-                    history_task["frames"] = sorted(history_task["frames"], key=lambda k: k["frame"])
+                    history_task['frames'].append(frame_item)
+                    history_task['frames'] = sorted(history_task['frames'], key=lambda k: k['frame'])
 
-                    render_frame = frame_item["frame"]
+                    render_frame = frame_item['frame']
 
                     json_manager.write(history_path, history_data)
                     print(task_translator.get_command(task, render_frame, tile=tile))
@@ -175,24 +175,26 @@ for task in settings_data["tasks"]:
 
 
 
+
     # for new task or empty history
-    history_item = {}
+    frame_item['frame'] = frameFrom
 
-    history_item["comments"]   = comments
-    history_item["katanaFile"] = katanaFile
-    history_item["renderNode"] = renderNode
 
-    history_item["var"] = var
-
-    frame_item["frame"] = frameFrom
-    history_item["frames"] = [frame_item]
+    # define history item
+    history_item = dict(
+        katanaFile=katanaFile,
+        renderNode=renderNode,
+        frames=[frame_item],
+        var=var,
+        complete=False,
+        reported=False,
+        comments=comments)
 
     history_data.append(history_item)
 
 
-    render_frame = frame_item["frame"]
 
     json_manager.write(history_path, history_data)
-    print(task_translator.get_command(task, render_frame, tile=tile))
+    print(task_translator.get_command(task, frameFrom, tile=tile))
 
     sys.exit()
